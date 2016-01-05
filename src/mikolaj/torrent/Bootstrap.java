26b79c6af8aebec3dd8ebe693bba553d06c61b12
Mikolaj Gogula <mikolaj.gogula@gmail.com>
2016-01-05 22:14:34 +0100

    Handle main exception.

--- src/mikolaj/torrent/Bootstrap.java
+++ src/mikolaj/torrent/Bootstrap.java
@@ -15,25 +15,40 @@ public class Bootstrap {
     public static final String APPLICATION_NAME = "TORrent";
     public static final String APPLICATION_VERSION = "0.1";
 
-    private static void runService() {
-        new mikolaj.torrent.communication.server.Bootstrap(10101, System.getProperty("user.home"));
+    private static void runCommunicationPackage() {
+        try {
+            new mikolaj.torrent.communication.server.Bootstrap(10101, System.getProperty("user.home"));
+        } catch (Exception ex) {
+            System.out.println("* ERROR *");
+            System.out.println("Error with starting the communication server.");
+            System.out.println("Please check that port 10101 is free or if you have permission to run the app.");
+            System.exit(1);
+        }
+    }
+
+    private static void runCommandPackage() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.print("Type your commmand or type help to list all commands: ");
+                Parser cliParser = new Parser(scanner.next(), Service.getInstance(), Parser.PARAMETER_PARSER_SCANNER);
+
+                if (!cliParser.isValid()) {
+                    Service.getInstance().printHelp();
+                } else {
+                    cliParser.perform();
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println("Application got an error with execute the command.");
+            runCommandPackage();
+        }
     }
 
     public static void main(String[] args) {
         System.out.println(String.format("%s v%s - Welcome!", APPLICATION_NAME, APPLICATION_VERSION));
 
-        runService();
-
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Type your commmand or type help to list all commands: ");
-            Parser cliParser = new Parser(scanner.next(), Service.getInstance(), Parser.PARAMETER_PARSER_SCANNER);
-
-            if (!cliParser.isValid()) {
-                Service.getInstance().printHelp();
-            } else {
-                cliParser.perform();
-            }
-        }
+        runCommunicationPackage();
+        runCommandPackage();
     }
 }
